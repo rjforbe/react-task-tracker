25b7eb7e1d2daf7f7992115034c719589b09dfe2
Jay Forbes <rjforbe@gmail.com>
2022-07-19 22:28:13 -0400

    initial_setup
    Using the new Header component and learned about defining defaultProps

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>
-        
-    </div>
+    <header>
+        <h1>{props.title}</h1>
+    </header>
   )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
 }
 
 export default Header
